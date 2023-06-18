db5513b976aa6100effd7c34872e4dabf064a3c3
Kevin Dietz <kdietz65@gmail.com>
2023-06-17 22:11:33 -0500

    Push changes to kevin branch

--- source/Eugene/Collections/Array/DiskArray.cs
+++ source/Eugene/Collections/Array/DiskArray.cs
@@ -34,7 +34,7 @@ public class DiskArray<TData> where TData : struct, IComparable
   {
     get
     {
-      GetAt(index, out var temp);
+      GetAt(index, out TData temp);
       return temp;
     }
   }
