d707d86e8a54199327956861c2921b43fbf0bad2
Kevin Dietz <kdietz65@gmail.com>
2023-06-18 15:36:33 -0500

    Add EugeneExplorer project start to examine contents of files created by Eugene

---
+++ source/EugeneExplorer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Eugene;
+
+Console.WriteLine("Welcome to Eugene Explorer");
+Console.WriteLine("--------------------------");
+
+while (true)
+{
+  Console.WriteLine("1. Create a new file");
+  Console.WriteLine("2. Open an existing file");
+  Console.WriteLine("3. Show existing data structures");
+  Console.WriteLine("4. Add a new data structure");
+  Console.WriteLine("5. Manage an existing data structure");
+  Console.WriteLine("X. Exit program");
+
+  string response = Console.ReadLine();
+
+  if (response.ToLower() == "x")
+  {
+    break;
+  }
+}
+
